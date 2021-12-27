b3681dd53d1659fa12b4f59c859313c3269dc88e
Amandozz <amanda.srisou@gmail.com>
2021-12-27 16:05:48 +1000

    initialize dropdown

---
+++ src/lib/dropdown.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "@emotion/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import PropTypes, { object } from "prop-types";
+
+/**
+ * Create dropdown
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Dropdown(props) {
+    const ref = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+    const { options = [], onChange, value } = props;
+
+    /**
+     * Open and close dropdown
+     * @param event
+     */
+    const switchIsOpen = (e) => {
+        e.preventDefault();
+        setIsOpen(!isOpen);
+    }
+
+    /**
+     * Send selected value to onChange Props
+     * @param event
+     */
+    const handleClick = (e) => {
+        const option = options.find(opt => opt.value === e.target.getAttribute("data-value"));
+        onChange(option.value);
+    }
+
+    useEffect(() => {
+        /**
+         * Close dropdown when click outside
+         * @param e
+         */
+        const checkIfClickedOutside = (e) => {
+            if (isOpen && ref.current && !ref.current.contains(e.target)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", checkIfClickedOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", checkIfClickedOutside);
+        }
+    }, [isOpen]);
+
+    return (
+        <div ref={ref}>
+            <Button type="text" name="button" onClick={switchIsOpen} data-testid="selected-value">
+                { !value ? <p>Options</p> : value }
+
+                { isOpen ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} /> }
+            </Button>
+
+            {
+                isOpen &&
+                (
+                    <Select data-testid="select" onClick={handleClick} width={ref.current && ref.current.offsetWidth}>
+                        {options.map((option) => (
+                            <Paragraph key={option.label} data-value={option.value} onClick={switchIsOpen}>
+                                {option.label}
+                            </Paragraph>
+                        ))}
+                    </Select>
+                )
+            }
+        </div>
+    );
+}
+
+Dropdown.propTypes = {
+    options: PropTypes.arrayOf(object).isRequired,
+    onChange: PropTypes.func,
+    value: PropTypes.string
+}
+
+Dropdown.defaultProps = {
+    options: [],
+}
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 4px;
+  background: #f1f2fa;
+  border: none;
+  width: 100%;
+  height: 40px;
+  cursor: pointer;
+`
+
+const Select = styled.div`
+  position: absolute;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 5px 10px 18px #888888;
+  width: ${props => props.width ? `${props.width}px` : '300px'};
+  height: 120px;
+  overflow: auto;
+  cursor: pointer;
+`
+
+const Paragraph = styled.p`
+  padding: 11px 16px;
+  margin: 0;
+
+  &:hover {
+    background: #f1f2fa;
+    border-radius: 4px;
+  }
+`
+
+export { Dropdown };
---
+++ src/lib/index.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Dropdown } from "./dropdown";
+import { render, fireEvent, screen } from "@testing-library/react";
+
+describe("Given dropdown is render with no options", () => {
+    test("Then the default option should be display", () => {
+        render(<Dropdown />)
+        expect(screen.getByTestId("selected-value")).toHaveTextContent('Options');
+    })
+})
+
+describe("Given dropdown is render with options", () => {
+    const options = [
+        { value: 'Sales', label: 'Sales' },
+        { value: 'Marketing', label: 'Marketing' },
+        { value: 'Engineering', label: 'Engineering' },
+        { value: 'Human Resources', label: 'Human Resources' },
+        { value: 'Legal', label: 'Legal' },
+    ];
+
+    test.each(options)("Then clicking on dropdown display %p", (option) => {
+        render(<Dropdown options={options} />)
+        fireEvent.click(screen.getByText('Options'))
+        expect(screen.getByTestId('select')).toHaveTextContent(option.label);
+    })
+
+    test("Then selecting an option call onChange with this option", () => {
+        const onChange = jest.fn();
+        render(<Dropdown options={options} onChange={onChange} />);
+        fireEvent.click(screen.getByText('Options'))
+        fireEvent.click(screen.getByText('Legal'))
+        expect(onChange).toHaveBeenCalledWith('Legal');
+    })
+
+    test("Then passing the props value display it as the selected option", () => {
+        render(<Dropdown options={options} value="Marketing" />);
+        expect(screen.getByTestId("selected-value")).toHaveTextContent('Marketing')
+        expect(screen.getByTestId("selected-value")).not.toHaveTextContent('Options');
+    })
+})
